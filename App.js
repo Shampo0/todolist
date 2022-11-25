59185804658da5bd7b66afc916e002e775676b1a
fhqhtdoszja <githubrobotncom@gmail.com>
2022-11-25 17:25:47 +0900

    three

--- App.js
+++ App.js
@@ -1,45 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback, Pressable ,TextInput, ScrollView} from 'react-native';
-import { theme } from './colors';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { Entypo } from '@expo/vector-icons'; 
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Alert,
+
+} from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { theme } from "./colors";
+
+const STORAGE_KEY="@toDos"
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
+  useEffect(()=>{
+    loadToDos();
+  }, []);
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
-  const addTodo = () => {
-    if(text === ""){
+  const saveToDos = async(toSave) => {
+    await AsyncStorage.setItem(STORAGE_KEY , JSON.stringify(toSave))
+  }
+  const loadToDos = async() =>{
+   const s = await AsyncStorage.getItem(STORAGE_KEY)
+    setToDos(JSON.parse(s))
+  }
+ 
+  const addToDo = async () => {
+    if (text === "") {
       return;
     }
-    const newToDos = {...toDos, [Date.now()]: {text, work: working},};
-      setToDos(newToDos);
-      setText("");
+    const newToDos = {
+      ...toDos,
+      [Date.now()]: { text, working },
+    };
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    setText("");
   };
-  console.log(toDos);
+  const deleteToDo = (key) =>{
+    Alert.alert(
+      "Delete To DO",
+      "Are you sure?",[
+        {text: "Cancel"},
+        {text: "I'm Sure" , onPress: async () => {
+          const newToDos = {...toDos};
+          delete newToDos[key];
+          setToDos(newToDos);
+          await saveToDos(newToDos);
+        },
+      },  
+        ]);
+        return;
+      };
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
         <TouchableOpacity onPress={work}>
-          <Text style={{...styles.btnText, color: working ? "white": theme.grey}}>Work</Text>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={travel} >
-           <Text style={{...styles.btnText, color: !  working ? "white": theme.grey}}>Travel</Text>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
         </TouchableOpacity>
       </View>
-      <View>
-        <TextInput onSubmitEditing={addTodo} onChangeText={onChangeText} returnKeyType="done" value={text} placeholderTextColor="red" placeholder={working ? "Add a To do" : "Where do you want to go?"} style={styles.input}></TextInput>
-        <ScrollView>{Object.keys(toDos).map((key) =>(
-          <View style={styles.toDo} key={key}>
-          <Text style={styles.toDoText}>{toDos[key].text}</Text>
-        </View>
+      <TextInput
+        onSubmitEditing={addToDo}
+        onChangeText={onChangeText}
+        returnKeyType="done"
+        value={text}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+        style={styles.input}
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key) => (
+          toDos[key].working === working  ? (<View style={styles.toDo} key={key}>
+            <Text style={styles.toDoText}>{toDos[key].text}</Text>
+            <TouchableOpacity onPress={() => deleteToDo(key)}>
+            <Entypo name="trash" size={24} color="red" />            
+            </TouchableOpacity>
+          </View>
+          ) : null
         ))}
-        </ScrollView>
-      </View>
-
+      </ScrollView>
     </View>
   );
 }
@@ -50,28 +112,36 @@ const styles = StyleSheet.create({
     backgroundColor: theme.bg,
     paddingHorizontal: 20,
   },
-  header:{
+  header: {
     justifyContent: "space-between",
-    flexDirection:"row",
+    flexDirection: "row",
     marginTop: 100,
   },
-  btnText:{
-    fontSize: 44,
-    fontWeight: '600',
-    color: "white",
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
   },
-  input:{
+  input: {
     backgroundColor: "white",
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
   },
-  toDo:{
-    color: "white",
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems:"center",
+    justifyContent: "space-between"
   },
-  toDoText:{
-
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
